d42a2e9d7d24272e457892aa9c08ef2b88c037e0
Renato Cruz Souza <renato.souza.flooxmongagua@gmail.com>
2025-03-28 13:25:37 -0300

    feat: add Primeiro component and render it in main file

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client';
 
+import Primeiro from './components/basic/Primeiro';
+
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
-const tag = <strong>Olá React!!</strong>
+
 
 root.render(<div>
-    {tag}
+ 
+ <Primeiro></Primeiro>
+ 
+
 </div>);
