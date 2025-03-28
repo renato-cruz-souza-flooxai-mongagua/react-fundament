847591437303575aac3dd05bd4491b421d548055
Renato Cruz Souza <renato.souza.flooxmongagua@gmail.com>
2025-03-28 12:58:34 -0300

    feat: initialize React app with basic rendering

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client';
+
+const el = document.getElementById('root');
+const root = ReactDOM.createRoot(el);
+const tag = <strong>Olá React!!</strong>
+
+root.render(<div>
+    {tag}
+</div>);
