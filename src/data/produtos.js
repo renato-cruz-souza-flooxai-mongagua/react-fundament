22e0c0f3d06df876e6303cf65198b518fb27d8ef
Renato Cruz Souza <renato.souza.flooxmongagua@gmail.com>
2025-04-01 11:33:52 -0300

    feat: add product table component with data and styling

---
+++ src/data/produtos.js
@@ -0,0 +1,11 @@
+const produtos = [
+    { id:1, nome: 'Caneta', preco: 2.99},
+    { id:2, nome: 'Lapis', preco: .99},
+    { id:3, nome: 'ipad', preco: 5899.99},
+    { id:4, nome: 'Motorola 53', preco: 6559.99},
+    { id:5, nome: 'Notebook', preco: 3400.99},
+    { id:6, nome: 'Caderno', preco: 19.99},
+
+]
+
+export default produtos
