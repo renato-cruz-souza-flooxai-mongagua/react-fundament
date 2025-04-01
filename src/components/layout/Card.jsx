6465adfb606db659f78804b7cedc6209cbd9590a
Renato Cruz Souza <renato.souza.flooxmongagua@gmail.com>
2025-04-01 11:48:59 -0300

    feat: add dynamic styling to Card component

--- src/components/layout/Card.jsx
+++ src/components/layout/Card.jsx
@@ -2,8 +2,14 @@ import './Card.css'
 import React from "react";
 
 export default props => {
+
+    const cardStyle = {
+        backgroundColor: props.color || '#f00',
+        borderColor : props.color || '#f00'
+    }
+
     return (
-    <div className="Card">
+    <div className="Card" style={cardStyle}>
         <div className='Title'>{props.titulo}</div>
         <div className='Content'>
             {props.children}
